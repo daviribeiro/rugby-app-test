4ef1da6c085a4d7fe52d8113719082e149533c18
daviribeiro <daviantunes27@hotmail.com>
2021-06-07 22:57:22 +0000

    Atualizacao de scripts

--- deletePodEvicted.sh
+++ deletePodEvicted.sh
@@ -2,8 +2,8 @@
 
 namespace="rugby-app-test"
 
-pod_evicted=`kubectl -n $namespace get po |grep master | grep Evicted | awk -F" " '{print $1}'`
-pod_terminating=`kubectl -n $namespace get po |grep master | grep Terminating | awk -F" " '{print $1}'`
+pod_evicted=`kubectl -n $namespace get po | grep Evicted | awk -F" " '{print $1}'`
+pod_terminating=`kubectl -n $namespace get po | grep Terminating | awk -F" " '{print $1}'`
 
 kubectl -n $namespace delete pods $pod_evicted
 kubectl -n $namepsace delete pods $pod_terminating
